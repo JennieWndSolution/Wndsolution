b95177545894f09836ca90688f56738cb9c5c7fc
JennieWndSolution <jennie_0114@hotmail.com>
2020-11-17 21:56:18 +0100

    removed unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,6 @@
-import Link from "next/link";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-  const items = [
-    { text: "1", link: "" },
-    { text: "2", link: "" },
-    { text: "3", link: "" },
-    { text: "4", link: "" },
-    { text: "4", link: "" },
-    { text: "4", link: "" },
-    { text: "4", link: "" },
-    { text: "4", link: "" },
-    { text: "4", link: "" },
-  ];
-
   return (
     <>
       <img
