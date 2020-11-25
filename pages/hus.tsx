1e1838af49c6a80f9893b7771e868a2b790855f9
JennieWndSolution <jennie_0114@hotmail.com>
2020-11-25 21:19:53 +0100

    add prop to object

--- pages/hus.tsx
+++ pages/hus.tsx
@@ -7,48 +7,62 @@ function Hus() {
       link: "https://fiskarhedenvillan.se/",
       img: "/fiskarhedenvillan.jpeg",
       alt: "Fiskarhedenvillan",
+      likable_houses: [
+        {
+          house_name: "Sidus",
+          house_link:
+            "https://fiskarhedenvillan.se/valj-ditt-hus/husmodeller/sidus/",
+        },
+      ],
     },
     {
       name: "Eksjöhus",
       link: "https://www.eksjohus.se/",
       img: "/eksjohus.jpeg",
       alt: "Eksjöhus",
+      likable_houses: [{ house_name: "", house_link: "" }],
     },
     {
       name: "Myrsjöhus",
       link: "https://www.myresjohus.se/",
       img: "/myrsjohus.jpeg",
       alt: "Myrsjöhus",
+      likable_houses: [{ house_name: "", house_link: "" }],
     },
     {
       name: "Anebyhus",
       link: "https://anebyhusgruppen.se/",
       img: "/anebyhus_vinkel_900x600.jpg",
       alt: "Anebyhus",
+      likable_houses: [{ house_name: "", house_link: "" }],
     },
     {
       name: "Götenehus",
       link: "https://gotenehus.se/",
       img: "/ekhem-2020-1344x731.jpg",
       alt: "Götenehus",
+      likable_houses: [{ house_name: "", house_link: "" }],
     },
     {
       name: "Älvsbyhus",
       link: "https://www.alvsbyhus.se/",
       img: "/alvsbyhus.jpg",
       alt: "Älvsbyhus",
+      likable_houses: [{ house_name: "", house_link: "" }],
     },
     {
       name: "Smålandsvillan",
       link: "https://www.smalandsvillan.se/",
       img: "/smalandsvillan.jpg",
       alt: "Smålandsvillan",
+      likable_houses: [{ house_name: "", house_link: "" }],
     },
     {
       name: "Trivselhus",
       link: "https://www.trivselhus.se/",
       img: "/trivselhus.jpg",
       alt: "Trivselhus",
+      likable_houses: [{ house_name: "", house_link: "" }],
     },
   ];
   return (
@@ -61,9 +75,13 @@ function Hus() {
             </a>
           </div>
           <div className={styles.body}>
-            {house.name} Exempel på hus vi gillar
+            {house.likable_houses.map((h, h_idx) => (
+              <div key={h_idx}>
+                Hus vi gillar: <a href={h.house_link}>{h.house_name}</a>
+              </div>
+            ))}
           </div>
-          <div className={styles.footer}>Lorem footer</div>
+          {/* <div className={styles.footer}>Lorem footer</div> */}
         </div>
       ))}
     </div>
